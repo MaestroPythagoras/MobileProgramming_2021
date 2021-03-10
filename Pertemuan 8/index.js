ec422c3d26dd619d369df185efa744b8e869cc96
Maestro Pythagoras <maestro.pythagoras@gmail.com>
2021-03-10 17:51:34 +0700

    Pertemuan 8 (15/02/2021)
    Destructuring

---
+++ Pertemuan 8/index.js
@@ -0,0 +1,9 @@
+const arrPerson = ["Maestro","Pythagoras", 20, true];
+
+const firstName = arrPerson[0];
+const lastName = arrPerson[1];
+const age = arrPerson[2];
+const isMarried = arrPerson[3];
+
+const [firstName, ,age] = arrPerson;
+console.log(firstName, age);
